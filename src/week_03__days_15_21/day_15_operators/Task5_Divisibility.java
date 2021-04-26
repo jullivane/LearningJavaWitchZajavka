5a41ea6c96dc3881769ba37f9d569c08cdeb59ce
jullivane <83136440+jullivane@users.noreply.github.com>
2021-04-26 17:33:29 +0200

    Update Task5_Divisibility.java

--- src/week_03__days_15_21/day_15_operators/Task5_Divisibility.java
+++ src/week_03__days_15_21/day_15_operators/Task5_Divisibility.java
@@ -4,7 +4,7 @@ package week_03__days_15_21.day_15_operators;
 //   czy metoda jest podzielna przed 3, przez 7 oraz przez 3 i 7 jednocześnie
 public class Task5_Divisibility {
 
-    public static void task_05__modulo_3_7(int value){
+    public static void divisible_3_7(int value){
         System.out.format("Czy liczba %d jest podzielna przez:\n", value);
         System.out.format("\t7?\t%b\n", value%7==0);
         System.out.format("\t3?\t%b\n", value%3==0);
@@ -12,11 +12,11 @@ public class Task5_Divisibility {
     }
 
     public static void main(String[] args) {
-        task_05__modulo_3_7(1);
-        task_05__modulo_3_7(3);
-        task_05__modulo_3_7(7);
-        task_05__modulo_3_7(21);
-        task_05__modulo_3_7(42);
-        task_05__modulo_3_7(43);
+        divisible_3_7(1);
+        divisible_3_7(3);
+        divisible_3_7(7);
+        divisible_3_7(21);
+        divisible_3_7(42);
+        divisible_3_7(43);
     }
 }
