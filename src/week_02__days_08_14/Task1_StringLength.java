fce40ef2eda74085ebefa18afd0f85de7055d3e0
jullivane <83136440+jullivane@users.noreply.github.com>
2021-04-27 20:08:33 +0200

    Update and rename Task1.java to Task1_StringLength.java

--- src/week_02__days_08_14/Task1_StringLength.java
+++ src/week_02__days_08_14/Task1_StringLength.java
@@ -3,11 +3,9 @@ package week_02__days_08_14;
 public class Task1 {
 // 1. Napisz program, który dla podanego Stringa przypisze wartość jego długości do zmiennej, następnie wyświetli jego długość na ekranie.
 
-    public static int task_01__text_length(String text){
-        return text.length();
-    }
-
     public static void main(String[] args) {
-        System.out.println(task_01__text_length("Ale ma kota"));
+        String text = "Ale ma kota";
+        int textLength = text.length();
+        System.out.println(textLength);
     }
 }
